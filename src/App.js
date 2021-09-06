79978f17a5e33a38793231b9a091000a8c4cc54f
Michail_N <mihasik323@gmail.com>
2021-09-06 18:12:29 +0300

    added basic components structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import 'normalize.css';
+import { Container } from "@material-ui/core";
+import "normalize.css";
+import { Redirect, Route, Switch } from "react-router";
 import "./App.css";
+import Chat from "./components/Chat/Chat";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <div className="App">Hello from frontend</div>;
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello from frontend</h1>
+        <Switch>
+          <Route path="chat">
+            <Chat />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,80 @@
+import clsx from "clsx";
+import { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    maxWidth: 414,
+    margin: "auto",
+  },
+  textField: {
+    width: "100%",
+  },
+  margin: {
+    margin: "0 auto",
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form className={clsx(classes.form)} autoComplete="off">
+        <TextField id="username" label="Имя пользователя" />
+        <TextField id="email" label="Email адрес" />
+        <FormControl className={clsx(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="standard-adornment-password">
+            Password
+          </InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
